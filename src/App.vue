55161516ae010d6d66cf03c1632e9a54f766a9df
mvtaheri <mvtaheri@gmail.com>
2021-07-19 12:44:39 +0430

    commit on buge vue-chart error

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+     <div class="row mt-5" v-if="arrPositive.length > 0">
+       <div class="col">
+          <h2>positive</h2>
+          <line-chart :label='label' :chartData="arrPositive" :options="chartOptions"></line-chart>
+       </div>
+     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import  axios from 'axios';
+import moment from 'moment';
+import LineChart from './components/LineChart.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    LineChart
+  },
+  data() {
+    return {
+      label: "Positive",
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
+        arrHospitalized:[],
+        arrPositive: [],
+        arrInIcu: [],
+        arrOnVentilator:[],
+        arrRecoverd:[],
+        arrDeaths:[],
+
+    }
+  },
+  async created() {
+         // https://covidtracking.com/api/us/daily
+         const {data}=await axios.get("http://localhost:8000/api/us/daily");
+         data.forEach(d => {
+           const date= moment(d.date,"YYYYMMDD").format("MMDD");
+           const {
+             positive,
+             hospitalizedCurrently,
+             inIcuCurrently,
+             onVentilatorCurrently,
+             recovered,
+             death
+           } = d;
+           this.arrPositive.push({date, total:positive});
+           this.arrHospitalized.push({date, total:hospitalizedCurrently});
+           this.arrInIcu.push({date, total:inIcuCurrently});
+           this.arrOnVentilator.push({date, total:onVentilatorCurrently});
+           this.arrRecoverd.push({date, total:recovered});
+           this.arrDeaths.push({date, total:death});
+         });
+    //  await axios.get("http://localhost:8000/events")
+    //    .then(response => (console.log(response.data)))
+    //  console.log(events);
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,24 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+    extends: Line,
+    props: {
+        label: {type: String},
+       chartData:{type: Array },
+       options:{type: Object}
+    },
+    mounted() {
+         const dates = this.chartData.map(d => d.date).reverse();
+         const totals= this.chartData.map(d => d.total).reverse();
+         console.log(totals);
+          this.renderChart({
+             labels: dates,
+             datasets: {
+                  label: this.label,
+                  data: totals
+             }
+         }, this.options)
+    },
+}
+</script>
